5af7d9c9f2ade19beded838de31c3ef758526ef5
Jintae Noh <kab7210@datasolution.kr>
2025-06-27 10:00:51 +0900

    Feat : 로그인 ui 컴포넌트 hook 추가, 적용

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,5 @@
-"use client";
-
-import oAuthSignIn from "@/utils/supabase/login";
+import Login from "./_components/Login";
 
 export default function LoginPage() {
-  const onClickGoogleLogin = async () => {
-    oAuthSignIn({
-      provider: "google",
-    });
-  };
-
-  return (
-    <div>
-      <button onClick={onClickGoogleLogin}>구글로 로그인</button>
-    </div>
-  );
+  return <Login />;
 }
