ad6a5e770d2b2a1bd656d2cb9b161bdd91c80516
jintae1224 <kab7210@naver.com>
2025-06-29 04:06:29 +0900

    Feat : Header 컴포넌트 생성, 적용

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,9 +1,21 @@
+import classNames from "classnames/bind";
+
+import Header from "@/app/_components/Header/Header";
 import UserProvider from "@/providers/UserProvider";
+
+import styles from "./layout.module.css";
+
+const cx = classNames.bind(styles);
 
 export default function AfterLoginLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <UserProvider>{children}</UserProvider>;
+  return (
+    <UserProvider>
+      <Header />
+      <div className={cx("container")}>{children}</div>
+    </UserProvider>
+  );
 }
