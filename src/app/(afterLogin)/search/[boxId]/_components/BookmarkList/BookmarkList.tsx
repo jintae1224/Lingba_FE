6e110cc255060cae98f64834575f962df8f1eb17
jintae1224 <kab7210@naver.com>
2025-07-14 23:06:56 +0900

    Feat : BookmarkGird 컴포넌트 생성, 적용

--- src/app/(afterLogin)/search/[boxId]/_components/BookmarkList/BookmarkList.tsx
+++ src/app/(afterLogin)/search/[boxId]/_components/BookmarkList/BookmarkList.tsx
@@ -3,59 +3,24 @@
 import classNames from "classnames/bind";
 
 import Breadcrumb from "@/app/_components/Breadcrumb/Breadcrumb";
-import { useBookmarkActions } from "@/hooks/search/useBookmarkActions";
+import { useAddFolderState } from "@/hooks/folder/useAddFolderState";
 
+import BookmarkGrid from "./BookmarkGrid/BookmarkGrid";
 import BookmarkHeader from "./BookmarkHeader/BookmarkHeader";
 import styles from "./BookmarkList.module.css";
-import FolderCard from "./FolderCard/FolderCard";
-import LinkCard from "./LinkCard/LinkCard";
 
 const cx = classNames.bind(styles);
 
 export default function BookmarkList() {
-  const {
-    boxId,
-    folders,
-    links,
-    breadcrumbs,
-    isLoading,
-    handleFolderClick,
-    handleCreateFolder,
-    handleDeleteFolder,
-  } = useBookmarkActions();
-
-  if (isLoading) {
-    return <div className={cx("loading")}>로딩 중...</div>;
-  }
+  const { isAddOn, handleAddOn, handleAddClose } = useAddFolderState();
 
   return (
     <div className={cx("container")}>
-      <Breadcrumb boxId={boxId} breadcrumbs={breadcrumbs} />
+      <Breadcrumb />
 
-      <BookmarkHeader onCreateFolder={handleCreateFolder} />
+      <BookmarkHeader handleAddOn={handleAddOn} />
 
-      <div className={cx("content")}>
-        <div className={cx("grid")}>
-          {folders.map((folder) => (
-            <FolderCard
-              key={folder.id}
-              folder={folder}
-              onFolderClick={handleFolderClick}
-              onDeleteFolder={handleDeleteFolder}
-            />
-          ))}
-
-          {links.map((link) => (
-            <LinkCard key={link.id} link={link} />
-          ))}
-        </div>
-
-        {folders.length === 0 && links.length === 0 && (
-          <div className={cx("empty-state")}>
-            북마크가 없습니다. 폴더나 링크를 추가해보세요.
-          </div>
-        )}
-      </div>
+      <BookmarkGrid isAddOn={isAddOn} handleAddClose={handleAddClose} />
     </div>
   );
 }
