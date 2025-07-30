cde592016570e8cce30eb833eb58bfaf5f50301d
jintae1224 <kab7210@naver.com>
2025-07-30 16:28:29 +0900

    Fix : Lnb -> Dock 컴포넌트 적용

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,10 +1,10 @@
 import classNames from "classnames/bind";
 
 import Footer from "@/app/_components/Footer/Footer";
-import Header from "@/app/(afterLogin)/_components/Header/Header";
-import Lnb from "@/app/(afterLogin)/_components/Lnb/Lnb";
 import UserProvider from "@/providers/UserProvider";
 
+import Dock from "./_components/Dock/Dock";
+import Header from "./_components/Header/Header";
 import styles from "./layout.module.css";
 
 const cx = classNames.bind(styles);
@@ -18,10 +18,10 @@ export default function AfterLoginLayout({
     <UserProvider>
       <Header />
       <div className={cx("layout")}>
-        <Lnb />
         <main className={cx("main-content")}>{children}</main>
       </div>
       <Footer />
+      <Dock />
     </UserProvider>
   );
 }
