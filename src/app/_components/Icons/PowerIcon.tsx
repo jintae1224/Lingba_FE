48da7b051aa352c1016c77f757ce0a2f5f9caf8f
jintae1224 <kab7210@naver.com>
2025-09-21 23:15:48 +0900

    Feat : power icon 컴포넌트 추가

---
+++ src/app/_components/Icons/PowerIcon.tsx
@@ -0,0 +1,28 @@
+import { IconProps } from "./types";
+
+export default function PowerIcon({
+  width = 24,
+  height = 24,
+  className,
+  strokeWidth = 2,
+  ...props
+}: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+      {...props}
+    >
+      <path d="M18.36 6.64a9 9 0 1 1-12.73 0" />
+      <line x1="12" y1="2" x2="12" y2="12" />
+    </svg>
+  );
+}
