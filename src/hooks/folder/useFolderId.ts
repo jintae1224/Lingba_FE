46fb951ba86aec57a6305a9ac6c05f8a8bd5ac04
Jintae Noh <kab7210@datasolution.kr>
2025-09-25 17:27:10 +0900

    Feat : folder id 관리 hook에 root 폴더 여부 추가

--- src/hooks/folder/useFolderId.ts
+++ src/hooks/folder/useFolderId.ts
@@ -5,5 +5,7 @@ import { useSearchParams } from "next/navigation";
 export function useFolderId() {
   const searchParams = useSearchParams();
   const folderId = searchParams.get("f_id");
-  return { folderId };
+  const isRootFolder = !folderId;
+
+  return { folderId, isRootFolder };
 }
